017a438c50d263f2617ab3e4791bed7ebe36677f
Eeyhan <1442478313@qq.com>
2019-07-28 15:26:36 +0800

    优化

--- proxy.py
+++ proxy.py
@@ -869,6 +869,12 @@ def save_redis(proxy_list, key=None):
     conn = redis.Redis(connection_pool=POOL)
     if not key:
         key = 'proxies'
+
+    # 检测是否已有值
+    cont = conn.get(key)
+    if cont:
+        cont = eval(cont)
+        proxy_list.extend(cont)
     conn.set(key, str(proxy_list))
 
 
